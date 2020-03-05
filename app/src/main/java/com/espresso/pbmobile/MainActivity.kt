361a2462b8eb512b31fef97a2cc76581f9f4b68a
mspc <marcin.szajna@reality.co>
2020-03-05 22:15:19 +0100

    ADD data binding

--- app/src/main/java/com/espresso/pbmobile/MainActivity.kt
+++ app/src/main/java/com/espresso/pbmobile/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.espresso.pbmobile
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.espresso.pbmobile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding by lazy(LazyThreadSafetyMode.NONE) {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding
     }
+
+
+
 }
---
+++ app/src/main/java/com/espresso/pbmobile/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.espresso.pbmobile.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.espresso.pbmobile.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
