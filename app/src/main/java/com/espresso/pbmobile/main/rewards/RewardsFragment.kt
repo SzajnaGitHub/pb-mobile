776261f398c00c9d3102f99a9f2b54175980215a
mspc <marcin.szajna@reality.co>
2020-03-23 23:23:26 +0100

    ADD rewards skeleton

--- app/src/main/java/com/espresso/pbmobile/main/rewards/RewardsFragment.kt
+++ app/src/main/java/com/espresso/pbmobile/main/rewards/RewardsFragment.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import com.espresso.pbmobile.R
+import com.espresso.pbmobile.databinding.FragmentRewardsBinding
 
 class RewardsFragment : Fragment() {
+    private lateinit var binding: FragmentRewardsBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_rewards, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_rewards, container, false)
+        setupRecycler()
+        return binding.root
+    }
+
+    private fun setupRecycler() {
+        binding.rewardsRecycler.apply {
+            layoutManager = GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
+
+        }
     }
 
     companion object {
